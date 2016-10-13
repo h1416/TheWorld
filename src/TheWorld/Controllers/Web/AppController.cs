6592154e9ccfa320c47648d0cdefc51148c05b5b
hcvo <jba152@att.net>
2016-10-13 15:25:07 -0500

    ++ Add App controller

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheWorld.Controllers.Wen
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
