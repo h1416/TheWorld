9f125eaf7b3955ef92f3611bddc62ba044fc3bbf
hcvo <jba152@att.net>
2016-10-13 14:54:31 -0500

    ++ add side bar toggle button ++ add side toggle even

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -2,17 +2,33 @@
 
 // self exec. function
 (function () {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Hieu Vo";
 
-    // change the background color of the form when hover over
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style.background = "#888";
-    }
+    //var $ele = $("#username");
+    //ele.text("Hieu Vo");
 
-    // restore the background color of the form 
-    main.onmouseleave = function () {
-        main.style.background = "";
-    }
+    //// change the background color of the form when hover over
+    //var main = $("#main");
+    //main.on("mouseenter",  function () {
+    //    main.style.background = "#888";
+    //});
+
+    //// restore the background color of the form 
+    //main.on("mouseleave", function () {
+    //    main.style.background = "";
+    //})
+
+    //var $menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        // toggleClass is adding the class if it doesn't exist and remove the class if it exists
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasclass)
+    })
+
 })();
