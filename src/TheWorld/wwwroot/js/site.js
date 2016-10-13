7cfec9724c543e30fc26d99768ddbe880289d3b6
hcvo <jba152@att.net>
2016-10-13 13:25:01 -0500

    ++ add bower config file ++ bring in JQuery ++ bring in underscore

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,1 +1,18 @@
 ﻿/*site.js*/
+
+// self exec. function
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Hieu Vo";
+
+    // change the background color of the form when hover over
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style.background = "#888";
+    }
+
+    // restore the background color of the form 
+    main.onmouseleave = function () {
+        main.style.background = "";
+    }
+})();
