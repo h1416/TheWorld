fd33755c240a73c1d6e3733cc56970909c98eaac
hcvo <jba152@att.net>
2016-10-14 15:06:01 -0500

    ++ Use MVC

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -16,13 +11,15 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles();
             app.UseStaticFiles();
+            
 
             loggerFactory.AddConsole();
 
@@ -31,7 +28,10 @@ namespace TheWorld
                 app.UseDeveloperExceptionPage();
             }
 
-
+            app.UseMvc(configRoute => configRoute.MapRoute(
+                name: "Default",
+                template: "{controller}/{action}/{id?}",
+                defaults: new { controller = "App", action = "index" }));
         }
     }
 }
