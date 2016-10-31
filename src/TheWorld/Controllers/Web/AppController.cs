2f0088eff30619b6fff797a6990191d660c76ee0
hcvo <jba152@att.net>
2016-10-31 13:35:47 -0500

    update project

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace TheWorld.Controllers.Wen
 {
     public class AppController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            throw new InvalidOperationException("bad thing happened");
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
