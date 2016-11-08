81c37ec26e3b8b6da9fffa7b4ca952ff966ea95a
hcvo <jba152@att.net>
2016-11-08 17:06:11 -0600

    ++ Inject IConfigurationRoot into the app controller ++ Add custom model state error ++ Add custom message to view bag ++ Read EmailSettings.Subject info from the config file ++ Read email settings.ToAddress from the environment variable

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
+using TheWorld.Services;
 using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Wen
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+        private readonly IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
         //Home
         public IActionResult Index()
         {
@@ -21,6 +31,23 @@ namespace TheWorld.Controllers.Wen
         [HttpPost]
         public IActionResult Contact(ContactViewModel vmContact)
         {
+            if (vmContact.Email.IndexOf('.') == -1)
+            {
+                ModelState.AddModelError("", "Email must contain one period.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Send email
+                _mailService.SendMail(_config["MailSettings:ToAddress"], vmContact.Email, _config["MailSettings:Subject"], vmContact.Message);
+
+                // Clear all values so that the email will not accidently sent once more
+                ModelState.Clear();
+
+                // Set the message notify user that email has been sent successfully
+                ViewBag.UserMessage = "Message Sent Successfully";
+            }
+            
             return View();
         }
 
