a49b26a2aa860efd1e0cf9b7057a7a7b8d8bda08
hcvo <jba152@att.net>
2016-11-16 17:20:05 -0600

    ++ Tell it to camel case json result fields' names ++ No need, it's camel cased by default

---
+++ src/TheWorld/Controllers/Api/TripsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripsController : Controller
+    {
+        private IWorldRepository _repository;
+
+        public TripsController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            return Ok(_repository.GetAllTrips());
+        }
+    }
+}
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using TheWorld.Models;
 using TheWorld.Services;
 
@@ -52,6 +53,10 @@ namespace TheWorld
             services.AddLogging();
 
             services.AddMvc();
+                //.AddJsonOptions(config =>
+                //{
+                //    config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                //}); // tell it to camel-case json results properties' names. No need any more, it is by default now
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
