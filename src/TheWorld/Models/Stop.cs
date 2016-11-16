f7b71bd18475aee69973a9bfa3b0f52227654d8e
hcvo <jba152@att.net>
2016-11-16 08:46:59 -0600

    ++Added the first models, stop and trip. and ready for entity framework CORE

---
+++ src/TheWorld/Models/Stop.cs
@@ -0,0 +1,11 @@
+﻿namespace TheWorld.Models
+{
+    public class Stop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Logitute { get; set; }
+        public int Order { get; set; }
+    }
+}
