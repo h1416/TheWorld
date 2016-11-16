2d8f9a8b097c168bbfc2f8ae939032d57caf69b8
hcvo <jba152@att.net>
2016-11-16 10:17:07 -0600

    ++Add arrival date time column to stop ++Correct a typo on the longitude field name ++Create a new migration to update db ++Update Db

--- src/TheWorld/Migrations/20161116161116_AddArrivalDateTimeField_CorrectAFieldNameToLongitude.Designer.cs
+++ src/TheWorld/Migrations/20161116161116_AddArrivalDateTimeField_CorrectAFieldNameToLongitude.Designer.cs
@@ -8,9 +8,10 @@ using TheWorld.Models;
 namespace TheWorld.Migrations
 {
     [DbContext(typeof(WorldContext))]
-    partial class WorldContextModelSnapshot : ModelSnapshot
+    [Migration("20161116161116_AddArrivalDateTimeField_CorrectAFieldNameToLongitude")]
+    partial class AddArrivalDateTimeField_CorrectAFieldNameToLongitude
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
---
+++ src/TheWorld/Migrations/20161116161116_AddArrivalDateTimeField_CorrectAFieldNameToLongitude.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheWorld.Migrations
+{
+    public partial class AddArrivalDateTimeField_CorrectAFieldNameToLongitude : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Logitute",
+                table: "Stops");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Arrival",
+                table: "Stops",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Stops",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Arrival",
+                table: "Stops");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Stops");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Logitute",
+                table: "Stops",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
