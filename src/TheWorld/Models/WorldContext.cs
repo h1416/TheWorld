2c0ee3f31baddf7c92648b5d48928bf197405b1f
hcvo <jba152@att.net>
2016-11-16 09:38:10 -0600

    ++ Inject the Iconfigurationroot to read the config.json file ++ Inject a DbContextOptions and passed it to the base DbContext constructor inorder to override the base Onconfiguring event correctly ++ Tell EF Core to use sqlserver and provide it an connection string that was stored in the config.json file

--- src/TheWorld/Models/WorldContext.cs
+++ src/TheWorld/Models/WorldContext.cs
@@ -1,15 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld.Models
 {
     public class WorldContext : DbContext
     {
-        public WorldContext()
-        {
+        private IConfigurationRoot _config;
+        private DbContextOptions _options;
 
+        public WorldContext(IConfigurationRoot config, DbContextOptions options)
+            : base(options)
+        {
+            _config = config;
+            _options = options;
         }
 
         public DbSet<Trip> Trips { get; set; }
         public DbSet<Stop> Stops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:WorldContextConnection"]);
+        }
     }
 }
