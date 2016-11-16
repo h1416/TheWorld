e13196457af8ba63fc04d7e27fa789ad396bed55
hcvo <jba152@att.net>
2016-11-16 10:17:31 -0600

    + Correct stop properties name

--- src/TheWorld/Models/Stop.cs
+++ src/TheWorld/Models/Stop.cs
@@ -1,11 +1,14 @@
-﻿namespace TheWorld.Models
+﻿using System;
+
+namespace TheWorld.Models
 {
     public class Stop
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public double Latitude { get; set; }
-        public double Logitute { get; set; }
+        public double Longitude { get; set; }
         public int Order { get; set; }
+        public DateTime Arrival { get; set; }
     }
 }
